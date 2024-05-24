70ff4ed85ba46cdcfa272ab32407604fc76277d0
Achyut Dahal <achutdahal14@gmail.com>
2024-05-24 11:40:47 -0400

    changes in database

--- config/db.js
+++ config/db.js
@@ -1,24 +1,9 @@
 
 
 
-// require('dotenv').config();
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT
-// });
-
-// module.exports = pool;
-
 require('dotenv').config();
 const { Pool } = require('pg');
-const { Sequelize } = require('sequelize');
 
-// Pool for direct pg usage
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -27,42 +12,4 @@ const pool = new Pool({
   port: process.env.DB_PORT
 });
 
-// Sequelize setup
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Important for Heroku
-    }
-  }
-});
-
-// Export both configurations
-module.exports = {
-  pool,
-  sequelize,
-  development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-};
+module.exports = pool;
